90c8b79dd9cd5cab5d8411f0a31e2e6a3be981f9
bfeitknecht <bfeitknecht@ethz.ch>
2023-10-26 15:26:39 +0200

    vault backup: 2023-10-26 15:26:39 from MacBooc Pro
    Affected files:
    homepage/dev/every-day.sh

---
+++ homepage/dev/every-day.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the directory containing the HTML file
+cd ~/homepage/dev
+
+# Get the current date in the desired format
+current_date=$(date +"%a. %d.%m.%Y")
+
+# Update the HTML file with the current date
+sed -i "s/<p>Last updated: .*<\/p>/<p>Last updated: $current_date<\/p>/" every-day.html
