3760f77be0e2af429b76ae401bcce782551d8fd1
bfeitknecht <bfeitknecht@ethz.ch>
2023-10-27 09:48:56 +0200

    WIP (added time)

--- homepage/dev/every-day.sh
+++ homepage/dev/every-day.sh
@@ -3,10 +3,8 @@
 # Navigate to the directory containing the HTML file
 cd ~/homepage/dev
 
-# Get the current date in the desired format
-current_date=$(date +"%a. %d.%m.%Y")
+# Get the current date and time
+now=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Update the HTML file with the current date
-sed -i "s/<p>Last updated: .*<\/p>/<p>Last updated: $current_date!<\/p>/" every-day.html
-
-
+# Update the every-day.html file with the current date and time
+sed -i "s/<span id=\"modified\">.*<\/span>/<span id=\"modified\">$now<\/span>/" every-day.html
