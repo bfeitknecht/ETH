bcf0890d7fdc459adc863b412f1e3ea651d3617a
bfeitknecht <bfeitknecht@ethz.ch>
2023-10-27 10:37:10 +0200

    let's pretend that fiasco never happened

--- homepage/dev/every-day.sh
+++ homepage/dev/every-day.sh
@@ -8,3 +8,12 @@ now=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Update the every-day.html file with the current date and time
 sed -i "s/<span id=\"modified\">.*<\/span>/<span id=\"modified\">$now<\/span>/" every-day.html
+
+# Add all changes to the staging area
+git add ./
+
+# Commit the changes with a commit message
+git commit -m "daily commit"
+
+# Push the changes to the upstream remote origin master branch
+git push -u origin master
