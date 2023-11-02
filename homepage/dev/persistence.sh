49a24ad485a673fcb0ae7b4160b43c0e712fe759
bfeitknecht <bfeitknecht@ethz.ch>
2023-11-02 10:18:55 +0100

    automations🔥

---
+++ homepage/dev/persistence.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# navigate to "dev/" 
+cd ~/homepage/dev/
+
+# remove the old crontab
+crontab -r
+
+# create a new crontab from "persistance-template.txt"
+crontab -e persistence-template.txt
+
